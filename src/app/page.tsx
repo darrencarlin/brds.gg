be4e624edc04744e631f1ac7cb8bb56923051e75
Darren Carlin <dazftw@gmail.com>
2024-11-28 01:09:26 -0500

    SEO

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,12 @@
 import { HomePage } from "@/components/home-page";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "BRDS.gg",
+  description:
+    "BRDS.gg is a website for tracking your gaming sessions and competing with friends via real-time leaderboards.",
+  keywords: "gaming, sessions, tracking, friends, competition, leaderboards",
+};
 
 export default function Home() {
   return <HomePage />;
